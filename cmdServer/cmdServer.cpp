c8fdb8982919ac9f6ef443b151f4505e0a66f75e
Alejandro Bordallo <alex.bordallo@gmail.com>
2016-10-22 20:10:36 +0100

    Dev (#5)
    * Add makefiles
    
    * Fix official servo polarities
    
    * Improve cmdServer and martyPython inerface
    
    - Fix code, indentation/style, improved comments/documentation
    - Prepare demo.py code
    
    * Fix documentation, remove unnecessary code

--- cmdServer/cmdServer.cpp
+++ cmdServer/cmdServer.cpp
@@ -26,25 +26,25 @@
 #define DEFAULT_STEPTIME  2.0
 #define DEFAULT_MOVETIME  1.0
 
-// command definitions.
-// would be nicer to have these in a cross-language configuration file
-#define CMD_HELLO 0
-#define CMD_MOVEKNEE  1
-#define CMD_MOVEHIP   2
-#define CMD_LEANSAGITTAL  3
-#define CMD_LEANSIDEWAYS  4
-#define CMD_STEP  5
-#define CMD_WALK  6
-#define CMD_EYES  7
-#define CMD_KICK  8
-#define CMD_MOVEJOINTS  9
-#define CMD_LIFTLEG 10
-#define CMD_LOWERLEG 11
-#define CMD_CELEBRATE 12
-#define CMD_HIPTOBESQUARE 13
-#define CMD_ROLLERSKATE 14
-#define CMD_ARMS 15
-#define CMD_KETTLE 16
+// TODO: Move to cross-language configuration file
+enum Commands {
+  CMD_HELLO = 0,
+  CMD_MOVEKNEE,
+  CMD_MOVEHIP,
+  CMD_LEANSAGITTAL,
+  CMD_LEANSIDEWAYS,
+  CMD_STEP,
+  CMD_WALK,
+  CMD_EYES,
+  CMD_KICK,
+  CMD_MOVEJOINTS,
+  CMD_LIFTLEG,
+  CMD_LOWERLEG,
+  CMD_CELEBRATE,
+  CMD_HIPTOBESQUARE,
+  CMD_ROLLERSKATE,
+  CMD_ARMS
+};
 
 #define CMD_LEFT  0
 #define CMD_RIGHT 1
@@ -227,8 +227,10 @@ int runCommand(martyrobot& robot, vector<uint8_t> data) {
       angle = data[4];
       if (data[3] == CMD_NEGATIVE)
         angle *= -1;
+#ifdef R_EYER
       printf("settinng second eye to %.2f\n", angle);
       robot.setServo(R_EYER, angle);
+#endif
     }
     break;
   }
@@ -246,16 +248,16 @@ int runCommand(martyrobot& robot, vector<uint8_t> data) {
 
     float angle = (float)data[2];
 
-    // generate a trajectory to move the knee
-    // first line is the robot's present position
+    // Generate a trajectory to move the knee
+    // First line is the robot's present position
     tSetpoints.clear();
-    tSetpoints.push_back(
-      tline);     // tline should already have one line, with 0.0 timecode and robot's present angles
+    tSetpoints.push_back(tline);
+    // tline should already have one line, with 0.0 timecode and robot's present angles
     if (data[1] == CMD_LEFT) {
       tline[1 + R_LKNEE] = tline[1 + R_RKNEE] + angle;
     } else if (data[1] == CMD_RIGHT) {
-      tline[1 + R_RKNEE] = tline[1 + R_LKNEE] -
-                           angle;     // note the negative for the right knee
+      // note the negative for the right knee
+      tline[1 + R_RKNEE] = tline[1 + R_LKNEE] - angle;
     }
 
 
@@ -269,29 +271,30 @@ int runCommand(martyrobot& robot, vector<uint8_t> data) {
 
   case CMD_LOWERLEG: {
     // This will lower whichever leg is higher to the ground
-    // initial quick implementation just thinks about the knees.
-    // in the future should consider hip angle too, then adjust knee angle only to get foot to floor
-    // this function will never change the sign of the knee angle
+    // Initial implementation just thinks about the knees.
+    // TODO: should consider hip angle too,
+    // then adjust knee angle only to get foot to floor
+    // This function will never change the sign of the knee angle
 
     printf("lower leg command\n");
 
     float movetime = DEFAULT_MOVETIME;
     if (data.size() >= 2) movetime = (float)data[1] / 10;
 
-    // save initial position to setpoints
+    // Save initial position to setpoints
     tSetpoints.push_back(tline);
 
     printf("angles: rknee: %.2f\tlknee: %.2f\n", robot.jangles[R_RKNEE],
            robot.jangles[R_LKNEE]);
     if (abs(robot.jangles[R_RKNEE]) > abs(robot.jangles[R_LKNEE])) {
-      // right knee is higher than left knee
+      // Right knee is higher than left knee
       if (robot.jangles[R_RKNEE] < 0) {
         tline[1 + R_RKNEE] = 0 - abs(robot.jangles[R_LKNEE]);
       } else {
         tline[1 + R_RKNEE] = abs(robot.jangles[R_LKNEE]);
       }
     } else {
-      // left knee is higher than right knee
+      // Left knee is higher than right knee
       if (robot.jangles[R_LKNEE] < 0) {
         tline[1 + R_LKNEE] = 0 - abs(robot.jangles[R_RKNEE]);
       } else {
@@ -318,47 +321,22 @@ int runCommand(martyrobot& robot, vector<uint8_t> data) {
     rollerSkate(robot);
   }
   case CMD_ARMS: {
-    /*
-      data_t tSetpoints, genTraj;
-      data_t tLegs, tArms, tEyes;
-      data_t tiLegs, tiArms, tiEyes;
-      deque<float> tline(robot.jangles);
-      tline.push_front(0);
-      tArms.push_back(tline);
-      setPointsArmsUp(tArms, 0, 200, BEAT*4);*/
     float angle1 = data[1];
     float angle2 = data[2];
-
     robot.setServo(R_RARM, angle1);
     robot.setServo(R_LARM, angle2);
-
-
-    }/*
-    case CMD_KETTLE:{
-      float time = (float)data[1]/10;
-
-      // generate a trajectory to move the knee
-      // first line is the robot's present position
-      tSetpoints.clear();
-      tSetpoints.push_back(tline);      // tline should already have one line, with 0.0 timecode and robot's present angles
-      setPointsKettle(tSetpoints, time);
-
-      interpTrajectory(tSetpoints, tInterp, 0.05);
-      runTrajectory(robot, tInterp);
-      break;
-    }
-    */
+  }
   }
   return 1;
 }
 
 
 int main() {
-  // start robot-y stuff
+  // Start robot-y stuff
   martyrobot robot;
   robot.init();
 
-  // little "hello" from the robot
+  // Little "Hello" from the robot
   data_t tSetpoints, tInterp;
   deque <float> tline(R_NUMJOINTS + 1, 0);
   tSetpoints.push_back(tline);
@@ -373,14 +351,14 @@ int main() {
   robot.setServo(R_EYES, EYESNORMAL);
 
 
-  // networking setup
+  // Networking setup
   struct sockaddr_in servaddr, clientaddr;
   socklen_t clilen;
   int port = PORT, sock, clisock;
   int nbytes;
   char buffer[256];
 
-  // create socket, initialise and listen
+  // Create socket, initialise and listen
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     printf("cannot create socket");
     return 0;
@@ -399,20 +377,22 @@ int main() {
   while (1) {
     printf("Socket initialised on port: %d. Waiting for incoming connection\n",
            port);
-    // accept incoming connection
+    // Accept incoming connection
     clilen = sizeof(clientaddr);
     clisock = accept(sock, (struct sockaddr*) &clientaddr, &clilen);
     if (clisock < 0)
       printf("ERROR on accept");
-    // set the client port to nonblocking, to allow the main loop to run without pausing for incoming data
-    // we assume that there is only one server running, and at the moment just one client connected at any given time
-    //fcntl(clisock, F_SETFL, O_NONBLOCK);
+    // Set the client port to nonblocking, to allow the main loop
+    // to run without pausing for incoming data
+    // We assume that there is only one server running,
+    // and just one client connected at any given time
+    // fcntl(clisock, F_SETFL, O_NONBLOCK);
 
     printf("Incoming connection accepted...\n");
-    //int error = 0;
-    //socklen_t len = sizeof (error);
-    //int retval;
-    //while ((retval = getsockopt (clisock, SOL_SOCKET, SO_ERROR, &error, &len ))==0){
+    // int error = 0;
+    // socklen_t len = sizeof (error);
+    // int retval;
+    // while ((retval = getsockopt (clisock, SOL_SOCKET, SO_ERROR, &error, &len ))==0){
     nbytes = read(clisock, buffer, 256);
 
     if (nbytes > 0) {
@@ -425,7 +405,7 @@ int main() {
       }
       printf("\n");
       runCommand(robot, dbytes);
-      //runCommand(robot, (uint8_t) buffer[0]);
+      // runCommand(robot, (uint8_t) buffer[0]);
 
     }
     //}
